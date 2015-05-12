5b800e35321e2cf3b18c111013ea9fa386df1ebb
NikoRizky <joxchill999@gmail.com>
2015-05-12 17:12:48 +0700

    UTS
    Maaf kalau salah pak , saya sudah mencoba gak mencontoh dan mencoba
    berfikir sendiri pak .

---
+++ TestKomputerNico.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Ritz on 12/05/2015.
+ */
+public class TestKomputerNico {
+    public static void main(String[] args) {
+
+        int Spesifikasi = 1;
+
+        if (Spesifikasi == 1){
+            System.out.println("Kecepatan Prosesor Komputer:    500");
+            System.out.println("Kecepatan Prosesor Notebook:    3500");
+            System.out.println("Kapasitas Memori:   4000");
+            System.out.println("Kapasitas Storage:  80000");
+            System.out.println("Kapasitas Baterai:  7000");
+            System.out.println("  ");
+            System.out.println("Notebook dioverclock dengan kecepatan prosesor 3000");
+            System.out.println("Notebook dioverclock dengan kecepatan prosesor 3200");
+        }else{
+            System.out.println("Bismillah");
+        }
+
+        }
+    }
